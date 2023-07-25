73b2a08844d9bd17457b6e621ef73eb01b0801a4
durgaprasad <durgaprasad.myway.s@gmail.com>
2023-07-25 21:49:42 +0530

    Add JsDoc package and Test it

---
+++ docs/test/test.js
@@ -0,0 +1,14 @@
+/** This is a description of the foo function. */
+function foo() {
+  return null;
+}
+
+/**
+ * Represents a book
+ * @constructor
+ * @param {string} title - The title of the book.
+ * @param {string} author - The author of the book.
+ */
+function Book(title, author) {
+  return null;
+}
